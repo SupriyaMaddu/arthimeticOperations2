8c0a9d40b72011c303b96da1733462d7a151ba11
Supriya Maddu <117802859+SupriyaMaddu@users.noreply.github.com>
2023-08-08 12:08:10 +0530

    Add files via upload

---
+++ arthimeticOperations2.py
@@ -0,0 +1,53 @@
+A = input()
+B = input()
+result = int(A) + int(B)
+print(result)
+
+A = int(input())
+B = int(input())
+
+print(A / B)
+
+#area of a rectangle
+
+length = input()
+length = int(length)
+breadth = input()
+breadth = int(breadth)
+area = length * breadth
+print(area)
+
+#perimeter of a rectangle
+
+length = input()
+length = int(length)
+breadth = input()
+breadth = int(breadth)
+perimeter = 2 * (length + breadth)
+print(perimeter)
+
+#percentage of boys
+
+girls_percentage = int(input())
+total_percentage = 100
+
+boys_percentage = total_percentage - girls_percentage
+print(boys_percentage)
+
+#kilometers to meters
+
+kilometer = input()
+kilometer = float(kilometer)
+conversion_to_meters = kilometer * 1000
+conversion_to_meters = int(conversion_to_meters)
+print(conversion_to_meters)
+
+#remainder
+
+dividend = int(input())
+divisor = int(input())
+
+quotient = dividend // divisor
+remainder = dividend % divisor
+
+print(remainder)
